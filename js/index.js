fc315e72c5ec9423bdffa2bd36ff5716dc900e9e
Sanjit <ssanjit856@gmail.com>
2022-02-20 20:25:37 +0530

    fixed responsiveness

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 const menu = document.querySelector("button.menu");
 const rightNav = document.querySelector("ul.right");
+if(menu && rightNav)
 menu.addEventListener("click",()=>{
 rightNav.classList.toggle("show");
 })
 const filterButton = document.querySelector(".filter-btn");
 const sidebar = document.querySelector("aside.sidebar");
+if(filterButton && sidebar)
 filterButton.addEventListener("click",()=>{
 sidebar.classList.toggle("show");
 })
